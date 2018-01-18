223248d108f4a006cb50108a5fa3a4f26cda5144
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2018-01-18 16:36:45 +1100

    Initial commit

---
+++ kill_a_thread/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * C# Program to Kill a Thread
+ */
+
+using System;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace kill_a_thread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ThreadingClass th = new ThreadingClass();
+            Thread thread1 = new Thread(th.DoStuff);
+            thread1.Start();
+            Console.WriteLine("Press any key to exit!!!");
+            Console.ReadKey();
+            th.Stop();
+            thread1.Join();
+        }
+    }
+
+
+}
